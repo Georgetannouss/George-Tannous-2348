13aac52579ac3dec15c0f6531d4823c20b51723e
Georgetannouss <123344617+Georgetannouss@users.noreply.github.com>
2023-04-10 21:49:58 -0500

    Update FinalProjectpt1.py

--- FinalProject/FinalProjectpt1.py
+++ FinalProject/FinalProjectpt1.py
@@ -5,36 +5,14 @@ import csv  # Used to read and write CSV/Excel files
 import datetime  # Used to get the current time
 
 
-def view_data(self):
-    # Read Manufacturer List CSV
-    with open('ManufacturerList.csv', 'r') as manufacturerlist_file:
-        manufacturerlist_reader = csv.reader(manufacturerlist_file)
-        for row in manufacturerlist_reader:
-            item = Item(int(row[0]), row[1], row[2], p_damaged=row[3])
-            self.items.append(item)
+def conv_date(date: str):
+    temp_date = [int(c) for c in date.split(sep='/')]
 
-    # Read Price List CSV
-    with open('PriceList.csv', 'r') as pricelist_file:
-        pricelist_reader = csv.reader(pricelist_file)
-        for row in pricelist_reader:
-            # Look up Item by matching id
-            for item in self.items:
-                if int(row[0]) == item.id:
-                    item.price = int(row[1])
-                    break
-
-    # Read Service Dates List CSV
-    with open('ServiceDatesList.csv', 'r') as sercivedatelist_file:
-        sercivedatelist_reader = csv.reader(sercivedatelist_file)
-        for row in sercivedatelist_reader:
-            # Looks up the item by the matching ID
-            for item in self.items:
-                if int(row[0]) == item.id:
-                    item.date = row[1]
-                    break
-
+    return [temp_date[2], temp_date[0], temp_date[1]]
 
 # Creating the class called Item
+
+
 class Item:
     # The following class is being used to store an items information
 
@@ -87,37 +65,40 @@ class Inventory:
             return id_sort.id
         i_sorted_items = sorted(self.items, key=sort)
 
-        # This will write each type into the Inventory CSV file
-        with open(f'Inventory.csv', 'w', newline='') as type_file:
+        # This will write each type into the LaptopInventory CSV file
+        for ty in types:
+            # Uses file name with the Type and ending 'Inventory.csv'
+            with open(f'{ty.capitalize()}Inventory.csv', 'w', newline='') as type_file:
                 type_writer = csv.writer(type_file)
 
                 for i_item in i_sorted_items:
-                    if i_item.type == type:
+                    if i_item.type == ty:
                         type_writer.writerow((i_item.id, i_item.manufacturer, i_item.price,
-                                            i_item.date, i_item.damaged))
+                                              i_item.date, i_item.damaged))
 
     # C - PastServiceDateInventory
     def pastinventory(self):
         # This is to get the current date. Used import datetime in the beginning for this
         currentdate = [
+            datetime.date.today().year,
             datetime.date.today().month,
-            datetime.date.today().day,
-            datetime.date.today().year
+            datetime.date.today().day
         ]
 
         # This is going to sort the inventory by dates
         def sort(date_sort: Item):
-            return date_sort.date
+            return conv_date(date_sort.date)
+
         date_sorted_items = sorted(self.items, key=sort)
 
         # This will write to the PastServiceDateInventory CSV file
         with open('PastServiceDateInventory.csv', 'w', newline='') as past_file:
             past_writer = csv.writer(past_file)
             for date_item in date_sorted_items:
-                # This will see if the date is past the due date by comparing the item date with the current date
-                if date_item.date < currentdate:
+                # This will see if the date is past the due date by comparing the item date with the current date]
+                if conv_date(date_item.date) < currentdate:
                     past_writer.writerow((date_item.id, date_item.manufacturer, date_item.type,
-                                        date_item.price, date_item.date, date_item.damaged))
+                                          date_item.price, date_item.date, date_item.damaged))
 
     # D - DamagedInventory
     def damagedinventory(self):
@@ -140,3 +121,39 @@ class Inventory:
         for item in self.items:
             print(item)
         print()
+
+
+inventory = Inventory()
+
+with open('ManufacturerList.csv', 'r') as ml_file:
+    ml_reader = csv.reader(ml_file)
+
+    for row in ml_reader:
+        inventory.items.append(Item(int(row[0]), row[1], row[2], p_damaged=row[3]))
+
+# Read Price List
+with open('PriceList.csv', 'r') as pl_file:
+    pl_reader = csv.reader(pl_file)
+
+    for row in pl_reader:
+        # Look up Item by matching id
+        for t_item in inventory.items:
+            if int(row[0]) == t_item.id:
+                t_item.price = int(row[1])
+                break
+
+# Read Service Dates List
+with open('ServiceDatesList.csv', 'r') as sdl_file:
+    sdl_reader = csv.reader(sdl_file)
+
+    for row in sdl_reader:
+        # Look up Item by matching id
+        for t_item in inventory.items:
+            if int(row[0]) == t_item.id:
+                t_item.date = row[1]
+                break
+
+inventory.fullinventory()
+inventory.inventorytype()
+inventory.pastinventory()
+inventory.damagedinventory()
