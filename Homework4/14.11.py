5e4d7eba295edce6d8922632d5348b31f57ff9ae
Georgetannouss <123344617+Georgetannouss@users.noreply.github.com>
2023-05-02 14:32:01 -0500

    Create 14.11.py

---
+++ Homework4/14.11.py
@@ -0,0 +1,19 @@
+#George Tannous 1971969
+#14.11
+
+def selection_sort_descend_trace(lst):
+    for i in range(len(lst)):
+        max_idx = i
+        for j in range(i + 1, len(lst)):
+            if lst[j] > lst[max_idx]:
+                max_idx = j
+        lst[i], lst[max_idx] = lst[max_idx], lst[i]
+        if i != len(lst) - 1:
+            print(' '.join(str(x) for x in lst),'')
+
+def main():
+    lst = [int(x) for x in input().split()]
+    selection_sort_descend_trace(lst)
+
+if __name__ == '__main__':
+    main()
