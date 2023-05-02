d73e3a3ae9f2d780935f253b23b13817a3bd5aeb
Georgetannouss <123344617+Georgetannouss@users.noreply.github.com>
2023-05-02 14:31:43 -0500

    Create 12.9.py

---
+++ Homework4/12.9.py
@@ -0,0 +1,14 @@
+#George Tannous 1971969
+#12.9
+
+parts = input().split()
+name = parts[0]
+while name != '-1':
+    try:
+        age = int(parts[1]) + 1
+    except ValueError:
+        age = 0
+    print(f'{name} {age}')
+
+    parts = input().split()
+    name = parts[0]
