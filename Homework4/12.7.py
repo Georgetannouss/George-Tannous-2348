16b6ebba81f5e6f4d662c9a6fd48cda8db448d43
Georgetannouss <123344617+Georgetannouss@users.noreply.github.com>
2023-05-03 17:02:13 -0500

    Update 12.7.py

--- Homework4/12.7.py
+++ Homework4/12.7.py
@@ -1,5 +1,5 @@
-#George Tannous 1971969
-#12.7
+# George Tannous 1971969
+# 12.7
 
 def get_age():
     age = int(input())
@@ -7,11 +7,13 @@ def get_age():
         raise ValueError("Invalid age.")
     return age
 
+
 def fat_burning_heart_rate(age):
     max_heart_rate = 220
     fat_burning_rate = 0.7
     heart_rate = (max_heart_rate - age) * fat_burning_rate
     return heart_rate
+
 
 if __name__ == "__main__":
     try:
@@ -20,3 +22,4 @@ if __name__ == "__main__":
         print(f"Fat burning heart rate for a {age} year-old: {heart_rate:.1f} bpm")
     except ValueError as e:
         print(f"{e}\nCould not calculate heart rate info.\n")
+
