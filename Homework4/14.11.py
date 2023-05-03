6e2165a3705684836c9709ee930f52151a46bcc6
Georgetannouss <123344617+Georgetannouss@users.noreply.github.com>
2023-05-03 17:04:02 -0500

    Update 14.11.py

--- Homework4/14.11.py
+++ Homework4/14.11.py
@@ -1,5 +1,5 @@
-#George Tannous 1971969
-#14.11
+# George Tannous 1971969
+# 14.11
 
 def selection_sort_descend_trace(lst):
     for i in range(len(lst)):
@@ -9,11 +9,13 @@ def selection_sort_descend_trace(lst):
                 max_idx = j
         lst[i], lst[max_idx] = lst[max_idx], lst[i]
         if i != len(lst) - 1:
-            print(' '.join(str(x) for x in lst),'')
+            print(' '.join(str(x) for x in lst), '')
+
 
 def main():
     lst = [int(x) for x in input().split()]
     selection_sort_descend_trace(lst)
+
 
 if __name__ == '__main__':
     main()
